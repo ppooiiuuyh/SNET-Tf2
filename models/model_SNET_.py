04b51396d5ed7d2875f46e8b5bdbb0590d0fc501
dohyun <ppooiiuuyh@naver.com>
2019-05-15 11:32:58 +0900

    f

--- models/model_SNET_.py
+++ models/model_SNET_.py
@@ -104,8 +104,9 @@ class Model_Train():
     # Typically, the test dataset is not large
     @tf.function
     def inference(self, input_image):
-        result = [g(input_image) for g in self.generators]
-        tf.print(result)
+        for i in range(8):
+            result = self.generators[i](input_image)
+            tf.print(result)
         return
 
     def test_step(self, test_dataset, summary_name = "test"):
