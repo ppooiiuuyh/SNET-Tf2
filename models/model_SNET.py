b59b0d7db67a1dea25b953015c2bc6e906c331ba
dohyun <ppooiiuuyh@naver.com>
2019-05-31 11:01:36 +0900

    d

--- models/model_SNET.py
+++ models/model_SNET.py
@@ -101,8 +101,10 @@ class Model_Train():
                 #label_image_test_crop = edge_crop(label_image_test), B_from_A = edge_crop(B_from_A)
                 #label_image_test_crop = cvt_ycbcr(label_image_test)[...,-1], B_from_A = cvt_ycbcr(B_from_A)[...,-1]
                 crop_pad = self.config.patch_size//2
-                PSNRs[e].append(tf.image.psnr(label_image_test[crop_pad:-crop_pad-1, crop_pad:-crop_pad-1],B_from_A.numpy()[crop_pad:-crop_pad-1, crop_pad:-crop_pad-1],1).numpy())
-                SSIMs[e].append(tf.image.ssim(label_image_test[crop_pad:-crop_pad-1, crop_pad:-crop_pad-1],B_from_A.numpy()[crop_pad:-crop_pad-1, crop_pad:-crop_pad-1],1).numpy())
+                A = tf.constant(label_image_test[crop_pad:-crop_pad - 1, crop_pad:-crop_pad - 1])
+                B = tf.constant(B_from_A.numpy()[crop_pad:-crop_pad - 1, crop_pad:-crop_pad - 1])
+                PSNRs[e].append(tf.image.psnr(A,B,1).numpy())
+                SSIMs[e].append(tf.image.ssim(A,B,1).numpy())
 
         """ log summary """
         if summary_name and self.step.numpy() %100 == 0:
