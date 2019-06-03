be177c0c32d39e9f01c5f1b3a62c87c7fe8ba1fb
dohyun <ppooiiuuyh@naver.com>
2019-06-03 17:22:35 +0900

    f

--- models/model_SNET.py
+++ models/model_SNET.py
@@ -80,7 +80,7 @@ class Model_Train():
 
 
         """ return log str """
-        return "g_loss : {} lr : {}".format(result_logs_dict["gen_loss"], self.learning_rate.numpy())
+        return "g_loss : {} lr : {}".format(result_logs_dict["gen_loss"], self.learning_rate().numpy())
 
 
 
